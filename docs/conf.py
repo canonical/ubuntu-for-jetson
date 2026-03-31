86e80d31f6670d486bacc74250bc48b9c1e738c8
Sebastian Weyer <sebastian.weyer@canonical.com>
2026-03-31 13:11:02 +0200

    fix: Use the correct baseurl for sphinx-sitemap.
    If we append `/en/latest`, the URLs in the generated sitemap will be of the
    form `/en/latest/latest` which will not be found.
    
    Therefore, remove that suffix from the baseurl.
    
    Signed-off-by: Sebastian Weyer <sebastian.weyer@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -184,7 +184,7 @@ html_theme_options = {
 
 # Base URL of RTD hosted project
 
-html_baseurl = 'https://canonical-ubuntu-for-jetson.readthedocs-hosted.com/en/latest/'
+html_baseurl = 'https://canonical-ubuntu-for-jetson.readthedocs-hosted.com/'
 
 # URL scheme. Add language and version scheme elements.
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
